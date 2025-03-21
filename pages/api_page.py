850ce3da4073348e7c7d87d330ec945d8438e8a2
nickz06 <31955147+nickz06@users.noreply.github.com>
2025-03-21 13:44:32 +0800

    Update and rename login_page.py to api_page.py

---
+++ pages/api_page.py
@@ -0,0 +1,17 @@
+# pages/login_page.py
+import time
+import random
+import datetime
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class ApiPage(BasePage):
+    
+    
+    def __init__(self,driver):
+        self.driver =driver
+
+    
+       
+        
